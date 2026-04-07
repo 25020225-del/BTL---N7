bfe90e6dc11c1fdcac84bbb243d169866c3247d9
khoailangngo <25020225@vnu.edu.vn>
2026-04-07 15:32:10 +0700

    add common directory

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -3,6 +3,10 @@ module org.example.demo {
     requires javafx.fxml;
     requires java.desktop;
     requires io.github.cdimascio.dotenv.java;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires common;
 
 
     opens org.example.demo to javafx.fxml;
