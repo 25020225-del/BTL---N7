fae9aaebdba9a8d3d8092a0acfb38867232f4ace
khoailangngo <25020225@vnu.edu.vn>
2026-04-07 13:29:08 +0700

    add common directory

--- common/src/main/java/model/Bidder.java
+++ common/src/main/java/model/Bidder.java
@@ -2,13 +2,14 @@ package model;
 
 public class Bidder extends User {
 
-    public Bidder() { super(); }
+    public Bidder() {
+        super();
+    }
 
     // Tiến hóa từ User thành Bidder
     public Bidder(User baseUser) {
         super(baseUser.getId(), baseUser.getUserName(), baseUser.getUserPass(), baseUser.getName(), "BIDDER");
-
-        // Dù Bidder không xài tới, nhưng copy sang cho đồng bộ với User gốc cũng không thừa
-        this.setGood(baseUser.isGood());
     }
 }
+
+        // Dù Bidder không xài tới, nhưng copy sang cho +
