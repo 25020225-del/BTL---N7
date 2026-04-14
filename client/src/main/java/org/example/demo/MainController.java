f70b257f2ffa28a8f6dd316dc85d7f6d7070ab3e
khoailangngo <25020225@vnu.edu.vn>
2026-04-14 18:48:42 +0700

    add common directory

--- client/src/main/java/org/example/demo/MainController.java
+++ client/src/main/java/org/example/demo/MainController.java
@@ -1,49 +1,46 @@
 package org.example.demo;
 
+import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML private TextField registerName;
+    @FXML private TextField registerAccountName;
+    @FXML private TextField registerPasswordAccount;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
-        FXMLLoader fxmlMainView = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
-        Scene sceneMainView = new Scene(fxmlMainView.load());
-        MainApplication.primalStage.setScene(sceneMainView);
+    protected void onLoginViewButtonClick() throws IOException {
+        MainApplication.setNewScene(MainApplication.rootLogin);
+    }
+
+    @FXML
+    protected void onRegisterViewButtonClick() throws IOException {
+        TextField registerName = (TextField) MainApplication.rootRegister.lookup("#registerName");
+        TextField registerAccountName = (TextField) MainApplication.rootRegister.lookup("#registerAccountName");
+        PasswordField registerPasswordAccount = (PasswordField) MainApplication.rootRegister.lookup("#registerPasswordAccount");
+        ComboBox<String> registerRole = (ComboBox<String>) MainApplication.rootRegister.lookup("#registerRole");
+        registerName.setText("");
+        registerAccountName.setText("");
+        registerPasswordAccount.setText("");
+        registerRole.setValue(null);
+        MainApplication.setNewScene(MainApplication.rootRegister);
+    }
+
+    @FXML
+    protected void onMainViewButtonClick() throws IOException {
+        MainApplication.setNewScene(MainApplication.rootMainView);
     }
 
     @FXML
     protected void onRegisterButtonClick() throws IOException {
         FXMLLoader fxmlRegister = new FXMLLoader(MainApplication.class.getResource("Register.fxml"));
         Scene sceneRegister = new Scene(fxmlRegister.load());
-    }
-
-    @FXML
-    protected void onLoginViewButtonClick() throws IOException {
-        FXMLLoader fxmlLoginView = new FXMLLoader(MainApplication.class.getResource("Login.fxml"));
-        Scene sceneLogin = new Scene(fxmlLoginView.load());
-        MainApplication.primalStage.setScene(sceneLogin);
-    }
-
-    @FXML
-    protected void onRegisterViewButtonClick() throws IOException {
-        FXMLLoader fxmlRegisterView = new FXMLLoader(MainApplication.class.getResource("Register.fxml"));
-        Scene sceneRegister = new Scene(fxmlRegisterView.load());
-        Parent root = sceneRegister.getRoot();
-        ChoiceBox<String> combo = (ChoiceBox<String>) root.lookup("#registerRole");
-        if (combo != null) {
-            combo.getItems().clear(); // Nên xóa danh sách cũ trước khi add để tránh bị lặp dữ liệu
-            combo.getItems().addAll("Bidder", "Seller", "Admin");
-        }
-        MainApplication.primalStage.setScene(sceneRegister);
     }
 }
