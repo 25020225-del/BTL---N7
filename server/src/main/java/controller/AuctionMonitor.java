a80abf9a55fef953bcb8cb4983a764b54b8d8fbd
25020256NguyenQuangManh <25020256@vnu.edu.vn>
2026-04-30 21:46:49 +0700

    chore: Add server's RAM cleaner for temporary auction saving

--- server/src/main/java/controller/AuctionMonitor.java
+++ server/src/main/java/controller/AuctionMonitor.java
@@ -1,15 +1,16 @@
 package controller;
 
-import model.Auction;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.List;
+
+import model.Auction;
+import server.ServerExtension.AuctionManager;
 
 import static utils.ConsoleColors.*;
 
 public class AuctionMonitor {
-
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private List<Auction> allAuctions;
 
@@ -18,7 +19,7 @@ public class AuctionMonitor {
     }
 
     public void startMonitoring() {
-        System.out.println("[Monitor]:" + GREEN + " The automatic auction monitoring system has been launched" + RESET);
+        System.out.println("[System]:" + GREEN + " Auction monitor has been launched." + RESET);
 
         scheduler.scheduleAtFixedRate(() -> {
             try {
@@ -26,9 +27,18 @@ public class AuctionMonitor {
                     if (auction.getStatus().equals(Auction.STATUS_RUNNING)) {
                         auction.closeAuctionIfTimeIsUp();
                     }
+
+                    String status = auction.getStatus();
+                    if (status.equals(Auction.STATUS_FINISHED) ||
+                        status.equals(Auction.STATUS_CANCELED) ||
+                        status.equals(Auction.STATUS_DELETED)) {
+                        allAuctions.remove(auction);
+                        AuctionManager.removeAuctionLock(auction.getId());
+                        System.out.println("[System]: " + BLUE + "Removed auction " + YELLOW + auction.getId() + RESET + " from RAM.");
+                    }
                 }
             } catch (Exception e) {
-                System.out.println("[Error]: Error during the bidding scan process: " + RED + e.getMessage() + RESET);
+                System.out.println("[System](AuctionMonitor): Error occurred during bidding scan process: " + RED + e.getMessage() + RESET);
                 e.printStackTrace();
             }
         }, 0, 10, TimeUnit.SECONDS);
@@ -36,7 +46,6 @@ public class AuctionMonitor {
 
     public void stopMonitoring() {
         scheduler.shutdown();
-        System.out.println(YELLOW + "[Monitor]: The auction monitoring system has been turned off" + RESET);
+        System.out.println("[System]: " + YELLOW + " Auction monitor has been shutdown." + RESET);
     }
-
 }
--- server/src/main/java/server/ServerExtension/AuctionManager.java
+++ server/src/main/java/server/ServerExtension/AuctionManager.java
@@ -1,17 +1,21 @@
 package server.ServerExtension;
 
 import model.Auction;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class AuctionManager {
-    private static final List<Auction> auctionList = new ArrayList<>();
+    private static final List<Auction> auctionList = new CopyOnWriteArrayList<>();
 
     private static final ConcurrentHashMap<String, Object> auctionLocks = new ConcurrentHashMap<>();
 
     public static Object getLockForAuction(String auctionId) {
         return auctionLocks.computeIfAbsent(auctionId, k -> new Object());
+    }
+
+    public static void removeAuctionLock(String auctionId){
+        auctionLocks.remove(auctionId);
     }
 
     public static synchronized void addAuctionToMonitor(Auction auction) {
